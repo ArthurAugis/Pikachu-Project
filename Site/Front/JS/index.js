4f4b3f2ea4a9e278c95a0320f1a16992c6f92486
Arthur <rainbowyyyoshi@gmail.com>
2023-12-09 15:20:33 +0100

    Ajoutez des fichiers projet.

---
+++ Site/Front/JS/index.js
@@ -0,0 +1,31 @@
+const pokemonButtons = document.querySelectorAll(".pokemon");
+const pokemonImg = document.querySelector("#pokemon-img");
+let selectedPokemonImage = null;
+
+pokemonButtons.forEach(function (button, index) {
+    button.addEventListener("mouseover", function () {
+        const newSelectedPokemonImage = button.getAttribute("data-image");
+        if (selectedPokemonImage !== newSelectedPokemonImage) {
+            selectedPokemonImage = newSelectedPokemonImage;
+            pokemonImg.src = selectedPokemonImage;
+
+            pokemonButtons.forEach(function (otherButton) {
+                if (otherButton !== button) {
+                    otherButton.classList.remove("selected");
+                }
+            });
+
+            button.classList.add("selected");
+        }
+    });
+});
+
+function updatePokemonImage() {
+    if (selectedPokemonImage !== null) {
+        pokemonImg.src = selectedPokemonIndex;
+    }
+}
+
+window.addEventListener("load", function () {
+    updatePokemonImage();
+});
